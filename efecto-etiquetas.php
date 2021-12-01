0160d4678cc340856d56a7c78c99b87764041bd4
AlejandroCabelloSizet <92577876+AlejandroCabelloSizet@users.noreply.github.com>
2021-12-01 12:53:18 +0100

    Create efecto-etiquetas.php

---
+++ efecto-etiquetas.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Etiquetas
+Plugin URI: https://alejandrocabello.com
+Description: Efecto etiquetas
+Version: 0.1
+Author: Alejandro Cabello
+Author URI: https://alejandrocabello.com
+*/
+
